b9438abe58376fea20f228681259172b60949077
Feng Jiang <fjiang91@gmail.com>
2019-04-22 16:44:54 -0400

    added express morgan and modified styling

--- app.js
+++ app.js
@@ -1,7 +1,46 @@
 const express = require("express");
+const morgan = require('morgan');
+const postBank = require('./postBank');
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World!"));
+
+app.use('*', (req, res, next) => {
+  // console.log('The user has performed some action');
+  next();
+});
+
+app.use(morgan('tiny'));
+app.use('/', express.static('public'));
+
+app.get("/", (req , res, next) => {
+  let posts = postBank.list();
+  let html = `<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+      ${posts.map(post => `
+        <div class='news-item'>
+          <p>
+            <span class="news-position">${post.id}. ▲</span>${post.title}
+            <small>(by ${post.name})</small>
+          </p>
+          <small class="news-info">
+            ${post.upvotes} upvotes | ${post.date}
+          </small>
+        </div>`
+      ).join('')}
+    </div>
+  </body>
+</html>`;
+
+  res.send(html);
+});
+
 
 const PORT = 1337;
 
