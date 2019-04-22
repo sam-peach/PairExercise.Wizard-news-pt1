e859dc86f7f8375a7bd1b72858ea5e79c68f80a3
sam-peach <sampeachart@gmail.com>
2019-04-22 17:14:19 -0400

    Sam's work

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 const express = require("express");
-const morgan = require('morgan');
-const postBank = require('./postBank');
+const morgan = require("morgan");
+const postBank = require("./postBank");
+const timeAgo = require("node-time-ago");
 const app = express();
 
-
-app.use('*', (req, res, next) => {
+app.use("*", (req, res, next) => {
   // console.log('The user has performed some action');
   next();
 });
 
-app.use(morgan('tiny'));
-app.use('/', express.static('public'));
+app.use(morgan("tiny"));
+app.use("/", express.static("public"));
 
-app.get("/", (req , res, next) => {
+app.get("/", (req, res, next) => {
   let posts = postBank.list();
   let html = `<!DOCTYPE html>
   <html>
@@ -23,7 +23,43 @@ app.get("/", (req , res, next) => {
   <body>
     <div class="news-list">
       <header><img src="/logo.png"/>Wizard News</header>
-      ${posts.map(post => `
+      ${posts
+        .map(
+          post => `
+        <div class='news-item'>
+          <p>
+            <span class="news-position">${post.id}. ▲</span><a href="/posts/${
+            post.id
+          }">${post.title}</a>
+            <small>(by ${post.name})</small>
+          </p>
+          <small class="news-info">
+            ${post.upvotes} upvotes | ${timeAgo(post.date)}
+          </small>
+          
+        </div>`
+        )
+        .join("")}
+    </div>
+  </body>
+</html>`;
+
+  res.send(html);
+});
+
+app.get("/posts/:id", (req, res, next) => {
+  // let posts = postBank.list();
+  const postId = +req.params.id;
+  let post = postBank.find(postId);
+  let html = `<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
         <div class='news-item'>
           <p>
             <span class="news-position">${post.id}. ▲</span>${post.title}
@@ -32,15 +68,13 @@ app.get("/", (req , res, next) => {
           <small class="news-info">
             ${post.upvotes} upvotes | ${post.date}
           </small>
-        </div>`
-      ).join('')}
+        </div>
     </div>
   </body>
 </html>`;
 
   res.send(html);
 });
-
 
 const PORT = 1337;
 
